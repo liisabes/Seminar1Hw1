7c46001def9f20a717aac9ed0eb972cb29df8148
liisabes <am8nl1492@gmail.com>
2022-09-19 01:38:44 +0300

    Add all files

---
+++ Exe1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа
+//и выдаёт, какое число большее, а какое меньшее.
+
+int firstNumber = -9;
+int secondNumber = -3;
+int max;
+max = firstNumber;
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("Первое число: " + firstNumber + " больше второго " + secondNumber);
+}
+else
+{
+    Console.WriteLine("Второе число: " + secondNumber + " больше первого " + firstNumber);
+}
+
---
+++ Exe2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+int firstNumber = 22;
+int secondNumber = 3;
+int thirdNumber = 9;
+int max;
+
+max = firstNumber;
+
+if (secondNumber > firstNumber)
+{
+     max = secondNumber;
+}
+if (thirdNumber > secondNumber)
+{
+        max = thirdNumber;
+}
+if (firstNumber > thirdNumber)
+{
+    max = firstNumber;
+}
+Console.WriteLine("Максимальное число: " + max);
+
